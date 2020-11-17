c6c5c51987e7b134440aad0c94646350393cc9e5
Saidy <saidasec9672@granitesd.org>
2020-11-16 18:47:00 -0700

    commment has been added

--- sketch.js
+++ sketch.js
@@ -1,4 +1,4 @@
-const Engine = Matter.Engine;
+Iconst Engine = Matter.Engine;
 const World= Matter.World;
 const Bodies = Matter.Bodies;
 
@@ -19,6 +19,7 @@ function setup(){
     ground = new Ground(600,height,1200,20);
     platform = new Ground(150, 305, 300, 170);
 
+	//box creation
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
     pig1 = new Pig(810, 350);
@@ -62,3 +63,8 @@ function draw(){
     bird.display();
     platform.display();
 }
+/
+	
+	
+
+
